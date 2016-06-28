bdd7e9c98d35be6f94d2e95812636ed8881294f1
Paulo de Freitas Coleta Neto <paulo.freitas@urucuia.grad>
2016-06-27 22:57:07 -0300

    Fix enconding problems.

--- dashboard/head.php
+++ dashboard/head.php
@@ -1,5 +1,5 @@
 <head>
-	<meta content="charset=utf-8">
+        <meta charset="utf-8" />
 	<title>Visualização de dados</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
