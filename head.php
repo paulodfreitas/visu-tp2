81435359b58d64e947916a6f25e120e1935273a9
juanoliveira11 <juanoliveira11@gmail.com>
2016-06-29 00:12:34 -0300

    Add files via upload
    tradução

---
+++ head.php
@@ -0,0 +1,47 @@
+<head>
+        <meta charset="utf-8" />
+	<title>Visualização de dados</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+  <!-- Syntax Highlighter -->
+  <link href="css/shCore.css" rel="stylesheet" type="text/css" />
+  <link href="css/shThemeDefault.css" rel="stylesheet" type="text/css" />
+  <!-- Demo CSS -->
+	<link rel="stylesheet" href="css/demo.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="flexslider.css" type="text/css" media="screen" />
+
+	<!-- Modernizr -->
+  <script src="js/modernizr.js"></script>
+
+  <script language="javascript" type="text/javascript">
+    function resizeIframe(obj) {
+      obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+    }
+  </script>
+
+  <style>
+  	
+  	.frame {
+  		border: 1px solid red;
+  		clear: both;
+  		width: 100%;
+  		transition: all 2s linear;
+  	}
+
+  </style>
+
+</head>
+
+<header>
+
+<ul>
+
+<li><a href="dash-3-principais-parceiros-comerciais.php">Business Partners</a></li>  
+<li><a href="dash-4-balanca-comercial.php">Balance of Trade (BOT)</a></li>  
+<li><a href="dash-5-empregos.php">Jobs</a></li>  
+<li><a href="dash-6-estados-cidades-mais-importantes.php">States and Cities</a></li>  
+
+</ul>
+  
+
+</header>
